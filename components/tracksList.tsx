08c270e56dec92dcbb541f16707f0fc8aedf1c3d
anydevlabs <avotiana@mail.hei.school>
2025-03-15 18:51:16 +0300

    build(dep): install @shopify/flash-list

--- components/tracksList.tsx
+++ components/tracksList.tsx
@@ -1,6 +1,23 @@
+import { FlashList } from "@shopify/flash-list"
+import { Text } from "react-native"
+
 export default function TracksList() {
+    const DATA = [
+        {
+            title: "First Item",
+        },
+        {
+            title: "Second Item",
+        },
+    ];
+    
     return (
         <>
+            <FlashList
+                data={DATA}
+                renderItem={({ item }) => <Text>{item.title}</Text>}
+                estimatedItemSize={200}
+            />
         </>
     )
 }
