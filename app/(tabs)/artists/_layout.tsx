8dad722f0e850a9a51b7b1df3004d8d0f6c58958
anydevlabs <andyavotiana@gmail.com>
2025-03-15 12:11:24 +0300

    feat(tab): change header and tab style

--- app/(tabs)/artists/_layout.tsx
+++ app/(tabs)/artists/_layout.tsx
@@ -1,10 +1,13 @@
+import { headerOptions } from '@/constants/layout'
 import { Stack } from 'expo-router'
 
 export default function ArtistsLayout() {
 	return (
 		<>
 			<Stack>
-				<Stack.Screen name="index" options={{ title: 'Artists' }} />
+				<Stack.Screen name="index" options={{ 
+					title: 'Artists',
+					...headerOptions }} />
 			</Stack>
 		</>
 	)
